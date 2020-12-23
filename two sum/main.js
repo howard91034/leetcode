e60970934ddfb1d48cc680799f221293e4465362
unknown <howard91034@gmail.com>
2020-12-23 22:20:34 +0800

    add two sum

---
+++ two sum/main.js
@@ -0,0 +1,19 @@
+var twoSum = function(nums, target) {
+    
+    var ans=[] ;
+   
+     for(var i=0;i<nums.length;i++){
+         for(var j=i+1;j<nums.length;j++){
+          if(nums[i]+nums[j]==target){
+             ans.push(i);   
+             ans.push(j);
+             
+             return ans;
+          }
+            
+         }
+         
+         
+     }
+    
+ }
